18b67e66d0e56fbff4ea9e4e5afe42b858d57e71
bitwisedhruv <monkcode463@gmail.com>
2024-04-09 02:18:39 +0530

    multiline statements

--- shell.py
+++ shell.py
@@ -2,9 +2,14 @@ import basica
 
 while True:
     text = input("> ")
+    if text.strip() == "":
+        continue
     result, error = basica.run("<stdin>", text)
 
     if error:
         print(error.as_string())
     elif result:
-        print(repr(result))
+        if len(result.elements) == 1:
+            print(result.elements[0])
+        else:
+            print(repr(result))
